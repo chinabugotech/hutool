861b961b6f50fe0748ffb0660f2bff902f7a1e9e
Looly <loolly@aliyun.com>
2025-04-21 18:22:48 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/v7/core/text/IssueI96LWHTest.java
+++ hutool-core/src/test/java/cn/hutool/v7/core/text/IssueI96LWHTest.java
@@ -24,6 +24,7 @@ public class IssueI96LWHTest {
 	@Test
 	public void replaceByCodePointTest() {
 		final String str = "\uD83D\uDC46最上方点击蓝字";
+		Assertions.assertArrayEquals(new int[]{128070, 26368, 19978, 26041, 28857, 20987, 34013, 23383}, str.codePoints().toArray());
 
 		// 这个方法里\uD83D\uDC46表示一个emoji表情，使用codePoint之后，一个表情表示一个字符，因此按照一个字符对
 		Assertions.assertEquals("\uD83D\uDC46最上下点击蓝字", StrUtil.replaceByCodePoint(str, 3, 4, "下"));
