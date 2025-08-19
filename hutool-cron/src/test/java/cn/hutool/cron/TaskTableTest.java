79361581ab89784aee5e4488cde94f9f250a3206
wqh <1241409260@qq.com>
2025-08-19 18:35:52 +0800

    Fix: 修复cron表达式，计算下一个日期错误

--- hutool-cron/src/test/java/cn/hutool/cron/TaskTableTest.java
+++ hutool-cron/src/test/java/cn/hutool/cron/TaskTableTest.java
@@ -1,10 +1,14 @@
 package cn.hutool.cron;
 
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.cron.pattern.CronPattern;
+import cn.hutool.cron.pattern.CronPatternUtil;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 public class TaskTableTest {
 
@@ -17,5 +21,16 @@ public class TaskTableTest {
 		taskTable.add(IdUtil.fastUUID(), new CronPattern("*/30 * * * * *"), ()-> Console.log("Task 3"));
 
 		Console.log(taskTable);
+	}
+	@Test
+	void testCron() {
+//        String cron = "0 0 0 */1 * ?";
+		String cron = "0 0 0 */1 * ? *";
+		DateTime judgeTime = DateTime.of(new Date());
+		CronPattern cronPattern = new CronPattern(cron);
+
+		System.out.println("cronPattern = " + cronPattern);
+		Date nextDate = CronPatternUtil.nextDateAfter(cronPattern, judgeTime, true);
+		System.out.println("nextDate = " + nextDate);
 	}
 }
