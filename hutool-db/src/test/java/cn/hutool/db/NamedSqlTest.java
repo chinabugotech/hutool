69789bac585a5b064851325b94691f417882f547
xxxtea <1027449630@qq.com>
2025-10-24 17:54:44 +0800

    feat:优化NamedSql对行构造器的处理 #4111

--- hutool-db/src/test/java/cn/hutool/db/NamedSqlTest.java
+++ hutool-db/src/test/java/cn/hutool/db/NamedSqlTest.java
@@ -5,9 +5,7 @@ import cn.hutool.db.sql.NamedSql;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -88,6 +86,31 @@ public class NamedSqlTest {
 	}
 
 	@Test
+	public void parseAnyTest(){
+		final HashMap<String, Object> paramMap = MapUtil.of("number", new int[]{1, 2, 3});
+
+		NamedSql namedSql = new NamedSql("select case when 2 = any(ARRAY[:number]) and 1 in (1) then 1 else 0 end;", paramMap);
+		assertEquals("select case when 2 = any(ARRAY[?,?,?]) and 1 in (1) then 1 else 0 end;", namedSql.getSql());
+		assertEquals(1, namedSql.getParams()[0]);
+		assertEquals(2, namedSql.getParams()[1]);
+		assertEquals(3, namedSql.getParams()[2]);
+	}
+
+	@Test
+	public void parseInsertMultiRowTest() {
+		// 多行 INSERT 语句
+		final Map<String, Object> paramMap = new LinkedHashMap<>();
+		paramMap.put("user1", new Object[]{1, "looly"});
+		paramMap.put("user2", new Object[]{2, "xxxtea"});
+
+		String sql = "INSERT INTO users (id, name) VALUES (:user1), (:user2)";
+		NamedSql namedSql = new NamedSql(sql, paramMap);
+
+		assertEquals("INSERT INTO users (id, name) VALUES (?,?), (?,?)", namedSql.getSql());
+		assertArrayEquals(new Object[]{1, "looly", 2, "xxxtea"}, namedSql.getParams());
+	}
+
+	@Test
 	public void queryTest() throws SQLException {
 		Map<String, Object> paramMap = MapUtil
 				.builder("name1", (Object)"王五")
@@ -109,9 +132,9 @@ public class NamedSqlTest {
 		final HashMap<String, Object> paramMap = MapUtil.of("info", new int[]{10, 20});
 
 		final NamedSql namedSql = new NamedSql(sql, paramMap);
-		// sql语句不包含IN子句，不会展开数组
-		assertEquals("select * from information where info_data = ?", namedSql.getSql());
-		assertArrayEquals(new int[]{10, 20}, (int[]) namedSql.getParams()[0]);
+		assertEquals("select * from information where info_data = ?,?", namedSql.getSql());
+		assertEquals(10, namedSql.getParams()[0]);
+		assertEquals(20, namedSql.getParams()[1]);
 	}
 
 	@Test
@@ -121,8 +144,8 @@ public class NamedSqlTest {
 		final HashMap<String, Object> paramMap = MapUtil.of("id", new int[]{5, 6});
 
 		final NamedSql namedSql = new NamedSql(sql, paramMap);
-		// sql语句不包含IN子句，不会展开数组
-		assertEquals("select * from user where comment = 'include in text' and id = ?", namedSql.getSql());
-		assertArrayEquals(new int[]{5, 6}, (int[]) namedSql.getParams()[0]);
+		assertEquals("select * from user where comment = 'include in text' and id = ?,?", namedSql.getSql());
+		assertEquals(5, namedSql.getParams()[0]);
+		assertEquals(6, namedSql.getParams()[1]);
 	}
 }
