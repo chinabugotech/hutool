1204a0033ef1c75aacb14c2300bd9fe3913713c9
Looly <loolly@aliyun.com>
2025-11-06 16:55:26 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/v7/core/io/file/Issue4121Test.java
+++ hutool-core/src/test/java/cn/hutool/v7/core/io/file/Issue4121Test.java
@@ -3,9 +3,7 @@ package cn.hutool.v7.core.io.file;
 import cn.hutool.v7.core.io.IORuntimeException;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Issue4121Test {
 	@Test
@@ -13,16 +11,6 @@ public class Issue4121Test {
 		assertThrows(IORuntimeException.class, () -> {
 			FileUtil.listFileNames("/non/existent/path");
 		});
-	}
-
-	@Test
-	void testListFileNames_RelativePath() {
-		// 测试相对路径（相对于classpath）
-		List<String> result = FileUtil.listFileNames("META-INF");
-
-		assertTrue(result.contains("MANIFEST.MF"));
-		assertTrue(result.contains("LICENSE-notice.md"));
-		assertTrue(result.contains("LICENSE.md"));
 	}
 
 }
