015f66ab711bb7bdc6f6b9ea1f450db26deae3d9
Looly <loolly@aliyun.com>
2026-07-06 10:40:51 +0800

    Validate `md5HexTo16` input length to avoid StringIndexOutOfBoundsException

--- hutool-crypto/src/main/java/cn/hutool/v7/crypto/digest/DigestUtil.java
+++ hutool-crypto/src/main/java/cn/hutool/v7/crypto/digest/DigestUtil.java
@@ -205,6 +205,9 @@ public class DigestUtil {
 	 * @since 4.4.1
 	 */
 	public static String md5HexTo16(final String md5Hex) {
+		if (md5Hex.length() < 24) {
+			throw new IllegalArgumentException("md5Hex length must be at least 24");
+		}
 		return md5Hex.substring(8, 24);
 	}
 
