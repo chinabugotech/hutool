ce5864bf228c96e84b5a3edcb44c36bd0de2003d
Looly <loolly@aliyun.com>
2026-08-03 17:23:16 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/v7/core/array/ArrayUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/v7/core/array/ArrayUtilTest.java
@@ -544,7 +544,7 @@ public class ArrayUtilTest {
 
 	@Test
 	public void lastIndexOfSubTest2() {
-		final Integer[] a = {0x12, 0x56, 0x78, 0x56, 0x21, 0x9A};
+		final Integer[] a = {0x12, 0x56, 0x78, 0x56, 0x78, 0x9A};
 		final Integer[] b = {0x56, 0x78};
 		final int i = ArrayUtil.lastIndexOfSub(a, b);
 		assertEquals(1, i);
